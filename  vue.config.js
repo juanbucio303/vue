73da3244657ce4fd75ecd28c23689f327f295e0b
juandres <juanbucio303@gmail.com>
2020-09-19 02:16:22 -0500

    update

---  vue.config.js
+++  vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
+  devServer: {
+    compress: true,
+    disableHostCheck: true,
+  },
+  publicPath: process.env.NODE_ENV === 'production'
       ? '/vue/' //nombre de tu proyecto GitHub
       : '/'
   }
